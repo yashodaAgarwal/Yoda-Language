46cd392b8d9103b58b27f8e539095f8280a23d12
yashodaAgarwal <55345522+yashodaAgarwal@users.noreply.github.com>
2021-11-17 14:39:46 +0530

    added index.html

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+var serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong with server. Try again after sometime !!")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value; //here i am taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // showing output
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
